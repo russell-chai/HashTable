b6b622a9c20461a1b599a729b341562143eaedb9
russell-chai <russell.chai@hotmail.com>
2018-04-18 10:56:24 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,31 +12,52 @@ using namespace std;
 int main() {
  
   tree* list = new tree();//tree
+ 
+  while (true) {
+   char* input = new char(20);
+   cout << "'input' to manual input" << endl;
+   cout << "'read' to read from file" << endl;
+   cout << "'print' to print out tree" << endl;
+   cout << "'quit' to quit" << endl;
+   cin.getline(input, 20);
+   if (strcmp(input, "input") == 0){
+    int number;
+    cin >> number;
+    cin.ignore();
+    list->insert(number, list->getHead());
+   }
+   if (strcmp(input, "read") == 0) {
+    cout << "input file name" << endl;
+    char* fileName = new char(200);
 
-  cout << "input file name" << endl;
-  char* fileName = new char(200);
+    cin.getline(fileName, 200);
+    ifstream fin(fileName);
 
-  cin.getline(fileName, 10);
-  ifstream fin(fileName);
-
-  int current = 0;
- //splits inputs by commas
-  while (!fin.eof()) {
-    char digit;
-    fin >> digit;
-    if (digit == ',') {
-      list->insert(current, list->getHead());
-      cout << "tree (sideways)" << endl;
-      list->print(list->getHead(), 0);
-      current = 0;
+    int current = 0;
+   //splits inputs by commas
+    while (!fin.eof()) {
+      char digit;
+      fin >> digit;
+      if (digit == ',') {
+        list->insert(current, list->getHead());
+        cout << "tree (sideways)" << endl;
+        list->print(list->getHead(), 0);
+        current = 0;
+      }
+      else {
+        current *= 10;
+        current += (digit - '0');
+      }
     }
-    else {
-      current *= 10;
-      current += (digit - '0');
-    }
+    current /= 10;
+    cout << "tree (sideways)" << endl;
+    list->insert(current, list->getHead());
+    list->print(list->getHead(), 0);
   }
-  current /= 10;
-  cout << "tree (sideways)" << endl;
-  list->insert(current, list->getHead());
-  list->print(list->getHead(), 0);
+  if (strcmp(input, "print") == 0) {
+    list->print(list->getHead(), 0);
+  }
+  if (strcmp(input, "quit") == 0) {
+    return 0;
+  }
 }
