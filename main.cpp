8caca1b40333e3afd2e8feb458712ec7afc24107
U-BSD\417856 <417856@SHS-10DCE0ADE.beaverton.k12.or.us>
2018-06-12 10:37:54 -0700

    first commit

--- main.cpp
+++ main.cpp
@@ -1,250 +1,223 @@
-/*
-Author: Russell Chai
-this program creates a graph, and allows user to add/delete nodes and edges
-also is able to return the shortest path between two nodes
-*/
 #include <iostream>
+#include <cstring>
 #include <vector>
-#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include <fstream>
 
 using namespace std;
-//struct for vertex
+
+struct student {
+  char* firstName;
+  char* lastName;
+  int ID;
+  double GPA;
+};
 struct node {
-  char* name;
-  vector<node*> connectTo;
-  vector<int> weights;
+  student* student;
+  node* next;
 };
-//used for djikstras
-//struct to hold the next node
-struct priority {
-  int length;
-  node* nextNode;
-  vector<node*> beenTo;
-};
-//if node from connects to find, returns the index of node find in from's connectTo vertex
-int ifContains(node* from, node* find) {
-  for (int a = 0; a < from->connectTo.size(); a++) {
-    if (from->connectTo[a] == find) {
-      return a;
-    }
-  }
-  return -1;
-}
-//print out adjacency table
-void print(vector<node*> allNodes) {
-  cout << "adjacency matrix" << endl;
-  cout << "\t";
-  for (int a = 0; a < allNodes.size(); a++) {
-    cout << allNodes[a]->name << "\t";
-  }
-  cout << endl;
-  for (int a = 0; a < allNodes.size(); a++) {
-    cout << allNodes[a]->name << "\t";
-    for (int b = 0; b < allNodes.size(); b++) {
+int getHash(int number, int size) {
+  int address = 0;
+  while (number != 0) {
+    address *= 13;
+    address += (number % 10);
+    address %= size;
 
-      if (ifContains(allNodes[a], allNodes[b]) != -1) {
-	cout << allNodes[a]->weights[ifContains(allNodes[a], allNodes[b])] << "\t";
-      }
-      else {
-	cout << "_" << "\t";
-      }
-    }
-    cout << endl;
+    number /= 10;
   }
+  address *= 13;
+  return address % size;
 }
-//returns the node in vertex allNodes, given the node's label
-node* getNode(char* label, vector<node*> allNodes) {
-  for (int a = 0; a < allNodes.size(); a++) {
-    if (strcmp(allNodes[a]->name, label) == 0) {
-      return allNodes[a];
-    }
+int getSize(node* start) {
+  int n = 0;
+  while(start->student != NULL) {
+    n++;
+    start = start->next;
   }
-  return NULL;
-}
-//shortest path algorithm
-int djikstra(vector<priority*> next, node* destination) {
-  //if there is nothing in next vertex, that means there is no path
-  //return -1
-  if (next.size() == 0) {
-    return -1;
-  }
-  //finds smallest path length
-  priority* smallest = next[0];
-  for (int a = 0; a < next.size(); a++) {
-    if (next[a]->length < smallest->length) {
-      smallest = next[a];
-    }
-  }
-  //removes priorty smallest from vertex
-  for (int a = 0; a < next.size(); a++) {
-    if (next[a] == smallest) {
-      next.erase(next.begin() + a);
-      break;
-    }
-  }
-
-  //if you reached the destination, return path length
-  if (smallest->nextNode == destination) {
-    return smallest->length;
-  }
-
-  //using current node, add priorities to next vertex
-  for (int a = 0; a < smallest->nextNode->connectTo.size(); a++) {
-    bool hasBeenTo = false;
-    for (int b = 0; b < smallest->beenTo.size(); b++) {
-      if (smallest->beenTo[b] == smallest->nextNode->connectTo[a]) {
-	hasBeenTo = true;
-	break;
-      }
-    }
-    if (hasBeenTo) {
-      continue;
-    }
-    priority* temp = new priority();
-    temp->nextNode = smallest->nextNode->connectTo[a];
-    temp->length = smallest->length + smallest->nextNode->weights[a];
-    for (int b = 0; b < smallest->beenTo.size(); b++) {
-      temp->beenTo.push_back(smallest->beenTo[b]);
-    }
-    temp->beenTo.push_back(smallest->nextNode);
-    next.push_back(temp);
-  }
-  return djikstra(next, destination);
+  return n;
 }
 int main() {
-  //vertex of all nodes
-  vector<node*> allNodes;
+  srand(time(NULL));
+  
+  int size = 100;
 
-  while(true) {
-    char* input = new char(20);
+  node *studentArray[size];
+  
+  for (int a = 0; a < size; a++) {
+    node* temp = new node();
+    temp->student = NULL;
+    temp->next = NULL;
+    studentArray[a] = temp;
+  }
+  node** students = studentArray;
+  
+  cout << "input list of first name file" << endl;
+  char* firstNameFile = new char(30);
+  cin.getline(firstNameFile, 30);
+  ifstream firstName(firstNameFile);
+  
+  cout << "input list of last name file" << endl;
+  char* lastNameFile = new char(30);
+  cin.getline(lastNameFile, 30);
+  ifstream lastName(lastNameFile);
+
+  vector<char*> firstNames;
+  vector<char*> lastNames;
+
+  while(!firstName.eof()) {
+    char* tempName = new char(30);
+    firstName.getline(tempName, 30);
+    firstNames.push_back(tempName);
+  }
+  while(!lastName.eof()) {
+    char* tempName = new char(30);
+    lastName.getline(tempName, 30);
+    lastNames.push_back(tempName);
+  }
+  
+  while (true) {
+    cout << "'a' to add student(s)" << endl;
+    cout << "'p' to print all the students out" << endl;
+    cout << "'d' to delete a student" << endl;
+    cout << "'q' to quit" << endl;
+
+    char input;
+    cin >> input;
+    cin.ignore();
+
     
-    cout << "input 'an' to add node" << endl;
-    cout << "input 'ae' to add edge" << endl;
-    cout << "input 'rn' to remove node" << endl;
-    cout << "input 're' to remove edge" << endl;
-    cout << "input 'spath' to find shortest path" << endl;
-    cout << "input 'quit' to quit" << endl;
-
-    cin.getline(input, 20);
-    //add node
-    if (strcmp(input, "an") == 0) {
-      cout << "input node's name" << endl;
-      node* temp = new node();
-      temp->name = new char(60);
-      cin.getline(temp->name, 60);
-      allNodes.push_back(temp);
-      print(allNodes);
-    }
-    //add edge
-    else if (strcmp(input, "ae") == 0) {
-      cout << "input first node's label" << endl;
-      char* first = new char(60);
-      cin.getline(first, 60);
-      cout << "input second node's label" << endl;
-      char* second = new char(60);
-      cin.getline(second, 60);
-
-      node* firstNode = getNode(first, allNodes);
-      node* secondNode = getNode(second, allNodes);
-      if(firstNode == NULL || secondNode == NULL) {
-	cout << "error: node(s) not found" << endl;
-	continue;
-      }
-      cout << "input edge weight" << endl;
-      int weight;
-      cin >> weight;
+    if (input == 'a') {
+      cout << "add by random student generater? (y/n)" << endl;
+      char yesRSG;
+      cin >> yesRSG;
       cin.ignore();
+      if (yesRSG == 'y') {
+	cout << "input first name file" << endl;
+	char* firstNameFile = new char(30);
+	cin.getline(firstNameFile, 30);
+	cout << "input last name file" << endl;
+	char* lastNameFile = new char(30);
+	cin.getline(lastNameFile, 30);
+
+	ifstream firstName(firstNameFile);
+	ifstream lastName(lastNameFile);
+	
+	cout << "input number of students you would like to generate" << endl;
+	int n;
+	cin >> n;
+	cin.ignore();
+
+	
+	while (n != 0) {
+	  student *temp = new student();
+	  
+	}
+      }
+      else if (yesRSG == 'n') {
+	cout << "input student's first name" << endl;
+	char* firstName = new char(30);
+	cin.getline(firstName, 30);
+
+	cout << "input student's last name" << endl;
+	char* lastName = new char(30);
+	cin.getline(lastName, 30);
+
+	cout << "input student's GPA" << endl;
+	double GPA;
+	cin >> GPA;
+	cin.ignore();
+	
+	cout << "input student's ID" << endl;
+	int ID;
+	cin >> ID;
+	cin.ignore();
+
+	student* temp = new student();
+	int hash = getHash(ID, size);
+	
+	temp->firstName = firstName;
+	temp->lastName = lastName;
+	temp->GPA = GPA;
+	temp->ID = ID;
+	
+	bool leave = false;
+	node* current = students[hash];
+	while(current->student != NULL) {
+	  if (current->student->ID == ID) {
+	    cout << "ID already used" << endl;
+	    leave = true;
+	    break;
+	  }
+	  current = current->next;
+	}
+	if (leave) {
+	  continue;
+	}
+	
+	while (getSize(students[getHash(ID, size)]) == 3) {
+	  node* newStudents[size * 2];
+
+	  for (int a = 0; a < size* 2; a++) {
+	    newStudents[a] = NULL;
+	  }
+	  for (int a = 0; a < size; a++) {
+	    node* current = students[a];
+	    while (current->student != NULL) {
+	      node* tempNew = new node();
+	      tempNew->student = current->student;
+	      tempNew->next = NULL;
+	      if (newStudents[getHash(current->student->ID, size * 2)] == NULL) {
+		newStudents[getHash(current->student->ID, size * 2)] = tempNew;
+	      }
+	      else {
+		node* tempCurrent = newStudents[getHash(current->student->ID, size * 2)];
+		while (tempCurrent->next != NULL) {
+		  tempCurrent = tempCurrent->next;
+		}
+		tempCurrent->next = tempNew;
+	      }
+	    }
+	  }
+	  size *= 2;
+	  
+	  students = newStudents;
+	}
+	if (students[getHash(ID, size)]->student == NULL) {
+	  node* add = new node();
+	  add->student = temp;
+	  add->next = NULL;
+	  students[getHash(ID, size)] = add;
+	}
+	else {
+	  node* current = students[getHash(ID, size)];
+	  while (current->next->student != NULL) {
+	    current = current->next;
+	  }
+	  node* add = new node();
+	  add->student = temp;
+	  add->next = NULL;
+	  current->next = add;
+	}
+      }
+    }
+    else if (input == 'p') {
+      for (int a = 0; a < size; a++) {
+	node* temp = students[a];
+	while(temp->student != NULL) {
+	  cout << temp->student->firstName << " " << temp->student->lastName << endl;
+	  cout << "GPA: " << temp->student->GPA << endl;
+	  cout << "ID: " << temp->student->ID << endl;
+	  temp = temp->next;
+	}
+	
+      }
+    }
+    else if (input == 'd') {
       
-      firstNode->connectTo.push_back(secondNode);
-      firstNode->weights.push_back(weight);
-      print(allNodes);
     }
-    //remove node
-    else if (strcmp(input, "rn") == 0) {
-      cout << "input the label of the node to remove" << endl;
-      char* label = new char(60);
-      cin.getline(label, 60);
-      node* toDelete = getNode(label, allNodes);
-      if (toDelete == NULL) {
-	cout << "error: node not found" << endl;
-	continue;
-      }
-      for (int a = 0; a < allNodes.size(); a++) {
-	int index = ifContains(allNodes[a], toDelete);
-	if (index != -1) {
-	  allNodes[a]->connectTo.erase(allNodes[a]->connectTo.begin() + index);
-	  allNodes[a]->weights.erase(allNodes[a]->weights.begin() + index);
-	}
-      }
+    else if (input == 'q') {
 
-      for (int a = 0; a < allNodes.size(); a++) {
-	if (allNodes[a] == toDelete) {
-	  allNodes.erase(allNodes.begin() + a);
-	  delete toDelete;
-	}
-      }
-      print(allNodes);
-    }
-    //remove edge
-    else if (strcmp(input, "re") == 0) {
-      cout << "input first node's label" << endl;
-      char* first = new char(60);
-      cin.getline(first, 60);
-      cout << "input second node's label" << endl;
-      char* second = new char(60);
-      cin.getline(second, 60);
-
-      node* firstNode = getNode(first, allNodes);
-      node* secondNode = getNode(second, allNodes);
-      if (firstNode == NULL || secondNode == NULL) {
-	cout << "error: node(s) not found" << endl;
-	continue;
-      }
-
-      int index = ifContains(firstNode, secondNode);
-      if (index == -1) {
-	cout << "error: nodes are not connected" << endl;
-	continue;
-      }
-
-      firstNode->connectTo.erase(firstNode->connectTo.begin() + index);
-      firstNode->weights.erase(firstNode->weights.begin() + index);
-      print(allNodes);
-    }
-    //get shortest path
-    else if (strcmp(input, "spath") == 0) {
-      cout << "input first node's label" << endl;
-      char* first = new char(60);
-      cin.getline(first, 60);
-      cout << "input second node's label" << endl;
-      char* second = new char(60);
-      cin.getline(second, 60);
-
-      node* firstNode = getNode(first, allNodes);
-      node* secondNode = getNode(second, allNodes);
-      if (firstNode == NULL || secondNode == NULL) {
-	cout << "error: node(s) not found" << endl;
-      }
-
-      vector<priority*> next;
-      for (int a = 0; a < firstNode->connectTo.size(); a++) {
-	priority* temp = new priority();
-	temp->length = firstNode->weights[a];
-	temp->nextNode = firstNode->connectTo[a];
-	temp->beenTo.push_back(firstNode);
-	next.push_back(temp);
-      }
-      int pathLength = djikstra(next, secondNode);
-      if (pathLength == -1) {
-	cout << "path does not exist" << endl;
-      }
-      else {
-	cout << "shortest path: " << pathLength << endl;
-      }
-    }
-    //quit
-    else if (strcmp(input, "quit") == 0) {
-      break;
     }
   }
 }
